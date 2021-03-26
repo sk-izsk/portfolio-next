25d26495cf9af0e3152e01615d22099cf0660cf8
Zeeshan <sk.zeeshan1992@gmail.com>
2021-03-25 21:20:20 -0400

    basic item list

---
+++ components/ListItem/ListItem.tsx
@@ -0,0 +1,27 @@
+interface Props {}
+
+const ListItem: React.FC<Props> = () => {
+  return (
+    <div className='flex flex-row items-center dark:bg-gray-800 bg-white text-purple-600 rounded-md p-2 border-2 shadow-inner dark:shadow-none dark:border-purple-600 text-sm'>
+      <div className='flex flex-1 h-full inline-flex bg-purple-600 text-white rounded-full h-6 px-3 justify-center items-center text-'>
+        Purple
+      </div>
+      <span className='inline-flex px-2'>
+        Aliquam fdgdcondimentum, odio ac finibus fermentum neque risus. Aliquam fdgdcondimentum,
+        odio ac finibus fermentum neque risus. Aliquam fdgdcondimentum, odio ac finibus fermentum
+        neque risus. Aliquam fdgdcondimentum, odio ac finibus fermentum neque risus. Aliquam
+        fdgdcondimentum, odio ac finibus fermentum neque risus. Aliquam fdgdcondimentum, odio ac
+        finibus fermentum neque risus. Aliquam fdgdcondimentum, odio ac finibus fermentum neque
+        risus. Aliquam fdgdcondimentum, odio ac finibus fermentum neque risus. Aliquam
+        fdgdcondimentum, odio ac finibus fermentum neque risus. Aliquam fdgdcondimentum, odio ac
+        finibus fermentum neque risus. Aliquam fdgdcondimentum, odio ac finibus fermentum neque
+        risus. Aliquam fdgdcondimentum, odio ac finibus fermentum neque risus. Aliquam
+        fdgdcondimentum, odio ac finibus fermentum neque risus. Aliquam fdgdcondimentum, odio ac
+        finibus fermentum neque risus. Aliquam fdgdcondimentum, odio ac finibus fermentum neque
+        risus. Aliquam fdgdcondimentum, odio ac finibus fermentum neque risus.
+      </span>
+    </div>
+  )
+}
+
+export { ListItem }
---
+++ pages/education.tsx
@@ -0,0 +1,15 @@
+import { Layout, ListItem } from '../components'
+
+interface Props {}
+
+const Education: React.FC<Props> = () => {
+  return (
+    <Layout>
+      <div className='container section-margin-top'>
+        <ListItem />
+      </div>
+    </Layout>
+  )
+}
+
+export default Education
